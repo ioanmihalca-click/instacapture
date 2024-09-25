9eca3650f86e8b6bf5f2662321b71675836b1f4d
Ioan "Click" Mihalca <164161041+ioanmihalca-click@users.noreply.github.com>
2024-09-25 23:07:34 +0300

    frontend

--- resources/views/livewire/acasa.blade.php
+++ resources/views/livewire/acasa.blade.php
@@ -1,4 +1,4 @@
-<div class="flex flex-col items-start justify-center max-w-3xl px-2 mx-auto mt-16 space-y-8 md:mt-24"
+<div class="flex flex-col items-start justify-center max-w-3xl px-2 mx-auto mt-16 space-y-4 md:mt-24"
      x-data="{ 
          professions: ['Paul Țuț','Fotograf', 'Editor Foto', 'Cameraman'],
          currentIndex: 0,
@@ -8,7 +8,7 @@
          setInterval(() => { currentIndex = (currentIndex + 1) % professions.length }, 3000);
          setTimeout(() => showContent = true, 500);
      ">
-     <div class="space-y-6 md:pr-24"
+     <div class="space-y-4 md:pr-24"
         x-cloak x-show="showContent"
          x-transition:enter="transition ease-out duration-1000"
          x-transition:enter-start="opacity-0 transform translate-y-4"
@@ -35,18 +35,18 @@
         </p>
     </div>
 
-    <div class="w-full space-y-6 md:pr-24"
+    <div class="w-full mt-4 space-y-4 md:pr-24"
     x-cloak x-show="showContent"
      x-transition:enter="transition ease-out duration-1000 delay-1000"
      x-transition:enter-start="opacity-0 transform translate-y-4"
      x-transition:enter-end="opacity-100 transform translate-y-0">
   
     <template x-for="(item, index) in [
-        {type: 'h3', content: 'Descoperă Portofoliul Meu', class: 'mt-1 text-2xl font-semibold text-yellow-400 uppercase md:text-3xl font-roboto-condensed'},
+        {type: 'h3', content: 'Descoperă Portofoliul Meu', class: 'text-2xl font-semibold text-yellow-400 uppercase md:text-3xl font-roboto-condensed'},
         {type: 'p', content: 'Te invit să explorezi <a class=\'text-yellow-400 hover:underline\' href=\'{{ route('portofoliu') }}\'>portofoliul meu</a>. Acesta îți va oferi o perspectivă asupra stilului și calității muncii mele.', class: 'text-base leading-relaxed text-white md:text-xl'},
-        {type: 'p', content: 'Pasiunea mea pentru fotografie se reflectă în fiecare imagine pe care o creez. Fie că este vorba despre o nuntă romantică, un eveniment corporate important sau o ședință foto de familie plină de veselie, mă dedic întotdeauna să surprind esența și emoția fiecărui moment.', class: 'mt-4 text-base leading-relaxed text-gray-300 md:text-xl'},
-        {type: 'a', content: 'Programează o Ședință Foto', class: 'inline-block px-6 py-2 my-6 text-xs font-bold text-black transition-colors duration-300 bg-yellow-400 rounded-full font-roboto-condensed md:text-base hover:bg-yellow-300', href: '{{ route('contact') }}'},
-        {type: 'p', content: 'Abordez fiecare proiect cu creativitate și profesionalism, asigurându-mă că rezultatele depășesc așteptările clienților mei. Utilizez echipamente de ultimă generație și tehnici avansate de editare pentru a livra fotografii de cea mai înaltă calitate.', class: 'mt-4 text-base leading-relaxed text-white md:text-xl'}
+        {type: 'p', content: 'Pasiunea mea pentru fotografie se reflectă în fiecare imagine pe care o creez. Fie că este vorba despre o nuntă romantică, un eveniment corporate important sau o ședință foto de familie plină de veselie, mă dedic întotdeauna să surprind esența și emoția fiecărui moment.', class: 'mt-2 text-base leading-relaxed text-gray-300 md:text-xl'},
+        {type: 'a', content: 'Programează o Ședință Foto', class: 'inline-block px-6 py-2 my-4 text-xs font-bold text-black transition-colors duration-300 bg-yellow-400 rounded-full font-roboto-condensed md:text-base hover:bg-yellow-300', href: '{{ route('contact') }}'},
+        {type: 'p', content: 'Abordez fiecare proiect cu creativitate și profesionalism, asigurându-mă că rezultatele depășesc așteptările clienților mei. Utilizez echipamente de ultimă generație și tehnici avansate de editare pentru a livra fotografii de cea mai înaltă calitate.', class: 'mt-2 text-base leading-relaxed text-white md:text-xl'}
     ]">
         <div x-cloak x-show="showContent"
              x-transition:enter="transition ease-out duration-1000"
@@ -89,7 +89,7 @@
             { icon: '📦', text: 'Fotografie de produs pentru e-commerce și cataloage locale' },
             { icon: '🎂', text: 'Fotografie pentru zile de naștere ale copiilor, captând bucuria în parcurile și locațiile distractive din Cluj si nu numai' }
         ]">
-            <li class="transition-all duration-500 ease-out opacity-0 bg-gradient-to-r from-gray-800 to-gray-900 rounded-lg p-4 shadow-md hover:shadow-lg hover:from-gray-700 hover:to-gray-800 transform hover:-translate-y-1"
+            <li class="p-4 transition-all duration-500 ease-out transform rounded-lg shadow-md opacity-0 bg-gradient-to-r from-gray-800 to-gray-900 hover:shadow-lg hover:from-gray-700 hover:to-gray-800 hover:-translate-y-1"
                 :class="{ 'opacity-100': showContent }"
                 :style="'transition-delay: ' + (1000 + (7 + index) * 300) + 'ms'">
                 <div class="flex items-center space-x-4">
